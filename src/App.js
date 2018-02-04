4718b0b873e34bf5bc4e3f5892b48ea489117d37
Jacob Hixon <jacobhixon47@gmail.com>
2018-02-04 04:21:06 -0600

    add (basic) links to menu. might look into react-router more

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebarIsVisible: false
+      sidebarIsVisible: false,
+      currentPage: 'CommandsList'
     };
 
     this.handleMenuToggle = this.handleMenuToggle.bind(this);
+    this.handleHomeClick = this.handleHomeClick.bind(this);
+    this.handleCommandsListClick = this.handleCommandsListClick.bind(this);
+
   }
 
   handleMenuToggle() {
@@ -19,8 +23,21 @@ class App extends Component {
     });
   }
 
+  handleHomeClick() {
+    this.setState({currentPage: 'Home'})
+  }
+
+  handleCommandsListClick() {
+    this.setState({currentPage: 'CommandsList'})
+  }
+
   render() {
-    let segmentContent = <CommandsList />;
+    let segmentContent;
+    if (this.state.currentPage === 'CommandsList') {
+      segmentContent = <CommandsList />;
+    } else if (this.state.currentPage === 'Home') {
+      segmentContent = <h1 style={{textAlign: 'center'}}>Welcome to AquaBot!</h1>;
+    }
 
     return (
       <div className="App">
@@ -33,13 +50,13 @@ class App extends Component {
               <Menu.Item name='aquaBot'>
                 <h2>AquaBot</h2>
               </Menu.Item>
-              <Menu.Item name='home'>
+              <Menu.Item name='home' onClick={this.handleHomeClick}>
                 <Icon name='home' />
                 Home
               </Menu.Item>
-              <Menu.Item name='gamepad'>
-                <Icon name='gamepad' />
-                Games
+              <Menu.Item name='commands' onClick={this.handleCommandsListClick}>
+                <Icon name='unordered list' />
+                Commands
               </Menu.Item>
               <Menu.Item name='camera'>
                 <Icon name='camera' />
@@ -49,7 +66,7 @@ class App extends Component {
             <Sidebar.Pusher>
               <Segment basic>
                 <Button onClick={this.handleMenuToggle} icon='content' style={{float: 'left'}}/>
-                <div style={{height: '90vh'}}>
+                <div style={{height: '100vh'}}>
                   {segmentContent}
                 </div>
               </Segment>
